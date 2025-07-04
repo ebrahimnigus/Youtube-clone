369c8759279fcaa89218f373c520363299d34b85
Ebrahim Ngus <ebrahimnigus9@gmail.com>
2025-07-04 23:57:53 +0300

    Update Video.jsx

--- src/pages/Video/Video.jsx
+++ src/pages/Video/Video.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Video.css'
-import PlayVideo from '../../Components/Playvideo/Playvideo'
+import PlayVideo from '../../Components/PlayVideo/PlayVideo'
 import Recommended from '../../Components/Recommended/Recommended'
 import { useParams } from 'react-router-dom'
 
